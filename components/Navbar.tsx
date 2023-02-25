8b4855f5866212b9d5cd49a6853ffea48e9950e0
MichOchieng <MichOchieng@gmail.com>
2023-02-25 11:55:31 -0800

    Added links to tabs

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Image, { StaticImageData } from 'next/image'
 import React from 'react'
 import windows from '../public/appIcons/windows.png'
 import fragz from '../public/appIcons/fragz.png'
+import Link from 'next/link'
 
 interface TabProps {
     img: StaticImageData,
@@ -31,10 +32,10 @@ const Navbar = () => {
 
   const Tab = ( { img, title }: TabProps ) => {
     return (
-        <span className='flex flex-row items-center min-w-fit px-1 py-4 h-full mr-3 border-2 border-b-black border-r-black overflow-hidden '>
+        <Link href={(title === 'Home') ? '/' : title.toLowerCase()} className='flex flex-row items-center min-w-fit px-1 py-4 h-full mr-3 border-2 border-b-black border-r-black overflow-hidden max-h-12 '>
             <Image className='w-6 h-6 mr-' src={img} alt='img'/>
             <h1 className='font-windows font-bold mx-1'>{title}</h1>
-        </span>
+        </Link>
     )
   }
 
