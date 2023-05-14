662e5955ee62bdbf14434d23ddce47bf11b66dde
Mich Ochieng <michochieng@gmail.com>
2023-05-13 23:54:46 -0700

    Removed pages directory and layout component

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,3 @@
-export { default as Layout } from './Layout'
 export { default as Navbar } from './Navbar'
 export { default as VideoPlayer } from './VideoPlayer'
 export { default as Card } from './Card'
