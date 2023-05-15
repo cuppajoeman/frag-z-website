2c3507410dee05af2d206183599db049357f1c88
MichOchieng <MichOchieng@gmail.com>
2023-05-14 18:06:25 -0700

    Added login dialogue

--- app/(user)/login/page.tsx
+++ app/(user)/login/page.tsx
@@ -1,7 +1,11 @@
+import LoginDialogue from '@/components/LoginDialogue'
 import React from 'react'
 
 export default function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+
+    return (
+        <div className="text-white">
+            <LoginDialogue/>
+        </div>
+    )
 }
