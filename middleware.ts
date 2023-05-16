b420e6d2ac50c194c8b71a9eeb823189742d693a
MichOchieng <MichOchieng@gmail.com>
2023-05-16 00:49:05 -0700

    Updated to add redirect to login/register page for active users

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,22 @@ import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
+import { Database } from "./lib/database.types";
 
 export async function middleware(req: NextRequest) {
     const res = NextResponse.next(); // create empty response
-    const supabase = createMiddlewareSupabaseClient({req, res}); // create supabase client
-    await supabase.auth.getSession(); // check if user is logged in
+    const pathname = req.nextUrl.pathname; // get pathname
+
+    const supabase = createMiddlewareSupabaseClient<Database>({req, res}); // create supabase client
+
+    const { data: session } = await supabase.auth.getSession(); // check if user is logged in
+
+    // If user is logged in and trying to access login or register page, redirect to home page
+    if (session && (pathname === "/login" || pathname === "/register")) {
+        return NextResponse.redirect("/");
+    }
+
     return res;
 }
+
+// Allows user session to be refreshed when navigating between pages
