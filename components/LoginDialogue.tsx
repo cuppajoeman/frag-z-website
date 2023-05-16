c15108ac92fef7608cdd82503b6730719ffe009c
MichOchieng <MichOchieng@gmail.com>
2023-05-15 20:24:47 -0700

    Added basic form structure

--- components/LoginDialogue.tsx
+++ components/LoginDialogue.tsx
@@ -1,15 +1,31 @@
 'use client'
 import React from 'react'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { Formik, Field, Form, ErrorMessage  } from 'formik'
 import { useSupabase } from '@/components/supabase/supabase-provider'
+import Image from 'next/image'
+
+import logo from "@/public/appIcons/fragz-logo.png";
+import discord from "@/public/images/discordlogo.png";
+import google from "@/public/images/googlelogo.png";
+import github from "@/public/images/githublogo.png";
+
+
 const LoginDialogue = () => {
     const { supabase } = useSupabase()
 
+    // Login logic
+
+    // Logout Logic
+
+    // SignUp logic
 
     const handleSignUp = async () => {
         await supabase.auth.signUp({
-            email: 'michochieng@gmail.com',
+            email: 'test@gmail.com',
             password: 'k}5|tY9Orq',
+            options: {
+
+            },
         })
             .then(res => {
                 console.log(res)
@@ -20,7 +36,7 @@ const LoginDialogue = () => {
     }
 
     const handleLogin = async () => {
-       await supabase.auth.signInWithPassword({
+        await supabase.auth.signInWithPassword({
             email: 'michochieng@gmail.com',
             password: 'k}5|tY9Orq',
         })
@@ -32,8 +48,8 @@ const LoginDialogue = () => {
             })
     }
 
-    const handleLogout =  async() => {
-       await supabase.auth.signOut()
+    const handleLogout = async () => {
+        await supabase.auth.signOut()
             .then(res => {
                 console.log(res)
             })
@@ -43,10 +59,50 @@ const LoginDialogue = () => {
     }
 
     return (
-        <div className='flex flex-col gap-4'>
-            <button onClick={handleLogin} className="">Sign In</button>
-            <button onClick={handleLogout} className="">Sign Out</button>
-            <button onClick={handleSignUp} className="">Sign up</button>
+        <div className='flex flex-col gap-4 w-[370px] md:w-[400px] h-[600px] items-center justify-start aid'>
+            {/* Logo header */}
+            <span className="logo-header">
+                <Image src={logo} alt='logo' className='object-cover' />
+            </span>
+            {/* Dialogue */}
+            <section className="">
+                {/* 3rd Party Login */}
+                <div className="flex flex-col w-[200px] h-[100px] text-white font-windows text-center justify-between">
+                    <h1 className="w-full text-2xl tracking-wider">Login With</h1>
+                    <div className="flex flex-row items-end justify-between w-full h-[60px]">
+                        <span className="auth-provider-button">
+                            <Image src={google} alt='logo' width={100} height={100} className='object-cover' />
+                        </span>
+                        <span className="auth-provider-button">
+                            <Image src={github} alt='logo' width={100} height={100} className='object-cover' />
+                        </span>
+                        <span className="auth-provider-button">
+                            <Image src={discord} alt='logo' width={100} height={100} className='object-cover' />
+                        </span>
+                    </div>
+                </div>
+                <h1 className="w-full text-white font-windows text-center text-2xl tracking-wider">or</h1>
+                {/* Email login */}
+                <div className="flex flex-col w-full h-[300px]">
+                    <Formik
+                        initialValues={{
+                            email: '',
+                            password: '',
+                        }}
+                        onSubmit={async (values) => {
+                            console.log(values);
+                        }}
+                    >
+                        <Form className='form'>
+                            <label className='form-label'>Email</label>
+                            <Field className='form-field' type="email" name="email" />
+                            <label className='form-label'>Password</label>
+                            <Field className='form-field' type="password" name="password" />
+                            <button type="submit" className='form-button'>Login</button>
+                        </Form>
+                    </Formik>
+                </div>
+            </section>
         </div>
     )
 }
