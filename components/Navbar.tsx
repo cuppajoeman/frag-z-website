17231be3349d6b510b3c0e0da7f411c5587e8bf9
MichOchieng <MichOchieng@gmail.com>
2023-05-17 04:08:59 -0700

    Added links based on user auth, updated tab icons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,12 @@ import Image, { StaticImageData } from "next/image";
 
 import fragz from "@/public/appIcons/fragz.png";
 import windows from "@/public/appIcons/windows.png";
+import userIcon from "@/public/images/defaultUser.png"
+import cartIcon from "@/public/images/cartIcon.png"
+import wikiIcon from "@/public/images/wikiIcon.png"
+import devIcon from "@/public/images/devIcon.png"
 
-import { useSupabase } from "./supabase/supabase-provider";
+import { useAuth } from "./supabase/supabase-auth-provider";
 
 interface TabProps {
   img: StaticImageData;
@@ -16,6 +20,7 @@ interface TabProps {
 }
 
 const Navbar = () => {
+  const { user } = useAuth();
 
   const tabdata: TabProps[] = [
     {
@@ -24,22 +29,22 @@ const Navbar = () => {
       link: "/",
     },
     {
-      img: fragz,
+      img: userIcon,
       title: "Login",
       link: "/login",
     },
     {
-      img: fragz,
+      img: cartIcon,
       title: "Store",
       link: "/store",
     },
     {
-      img: fragz,
+      img: devIcon,
       title: "Development",
       link: "/development",
     },
     {
-      img: fragz,
+      img: wikiIcon,
       title: "Wiki",
       link: "/wiki",
     },
@@ -47,15 +52,39 @@ const Navbar = () => {
 
   const Tab = ({ img, title, link }: TabProps) => {
     return (
-      <Link
-        href={link}
-        className={`flex flex-row items-center justify-start ${
-          title === "Home" ? "" : "w-[140px]"
-        } min-w-fit px-1 py-4 h-full mr-3 border-2 border-b-black border-r-black overflow-hidden max-h-12 `}
-      >
-        <Image className="w-6 h-6 mr-" src={img} alt="img" />
-        <h1 className="font-windows aid font-bold mx-1">{title}</h1>
-      </Link>
+      <>
+        {/* Only render home login and store if user isnt logged in */}
+        {(user === null && title === "Development" || title === "Wiki") ?
+          <>
+          </>
+          :
+          <>
+            {/* If users logged in show user tab instead of login tab */}
+            {(user!== null && title === "Login") ?
+              <>
+                <Link
+                  href={link}
+                  className={`flex flex-row items-center justify-start w-[140px] min-w-fit px-1 py-4 h-full mr-3 border-2 border-b-black border-r-black overflow-hidden max-h-12 `}
+                >
+                  <Image className="w-6 h-6" src={img} width={100} height={100} alt="img" />
+                  <h1 className="font-windows font-bold mx-1">{user.username}</h1>
+                </Link>
+              </>
+              :
+              <>
+                <Link
+                  href={link}
+                  className={`flex flex-row items-center justify-start ${title === "Home" ? "" : "w-[140px]"
+                    } min-w-fit px-1 py-4 h-full mr-3 border-2 border-b-black border-r-black overflow-hidden max-h-12 `}
+                >
+                  <Image className="w-6 h-6" src={img} width={100} height={100} alt="img" />
+                  <h1 className="font-windows font-bold mx-1">{title}</h1>
+                </Link>
+              </>
+            }
+          </>
+        }
+      </>
     );
   };
 
