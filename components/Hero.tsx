c2bdf94eda07904eab0124314dfe8fccc7182111
Mich Ochieng <michochieng@gmail.com>
2023-05-19 12:15:43 -0700

    First commit

---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className=''>
+        <div className="">
+            {/* Video block */}
+            <div className="">
+
+            </div>
+            {/* Text Block */}
+            <div className="">
+
+            </div>
+        </div>
+        {/* Alert row */}
+    </section>
+  )
+}
+
+export default Hero
---
+++ components/PropertiesWindow.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PropertiesWindow = () => {
+  return (
+    <div className=''>
+        
+    </div>
+  )
+}
