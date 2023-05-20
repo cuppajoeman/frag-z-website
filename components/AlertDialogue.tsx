dc90dd6c0cabae9a1b4738d67c8dbd72ba8031b7
MichOchieng <MichOchieng@gmail.com>
2023-05-20 05:23:48 -0700

    Styling changes

--- components/AlertDialogue.tsx
+++ components/AlertDialogue.tsx
@@ -7,31 +7,31 @@ interface Props {
     message: string;
 }
 
-const AlertDialogue = ( { title , message }: Props ) => {
-  return (
-    <div className='flex flex-col w-[300px] h-[200px] bg-[#C5C5C5] border-2 border-b-black border-r-black p-1 mx-auto'>
-        {/* Gradient */}
-        <span className="w-full h-7 mb-1 bg-gradient-to-r from-[#020C67] to-[#3372BC] flex flex-row items-center relative text-white justify-between font-windows text-sm tracking-widest p-1">
-            {title}
-            <span className="h-[20px] w-[20px] bg-[#C5C5C5] border-2 border-b-black border-r-black flex items-center justify-center text-black">
-            X
+const AlertDialogue = ({ title, message }: Props) => {
+    return (
+        <div className='flex flex-col w-[250px] lg:w-[300px] h-[200px] bg-[#C5C5C5] border-2 border-b-black border-r-black p-1 mx-auto'>
+            {/* Gradient */}
+            <span className="w-full h-7 mb-1 bg-gradient-to-r from-[#020C67] to-[#3372BC] flex flex-row items-center relative text-white justify-between font-windows text-sm tracking-widest p-1">
+                {title}
+                <span className="h-[20px] w-[20px] bg-[#C5C5C5] border-2 border-b-black border-r-black flex items-center justify-center text-black">
+                    X
+                </span>
             </span>
-        </span>
-        {/* Content */}
-        <section className="h-[100px] my-2 flex justify-evenly items-center flex-row w-full">
-        {/* Icon */}
-        <span className="w-[65px] h-[65px] min-w-[65px]">
-            <Image src={alert} alt='alert' className='object-cover' width={100} height={100}/>
-        </span>
-        {/* Message */}
-        <p className="font-windows">{message}</p>
-        </section>
-        {/* Button */}
-        <button className="button-v2 mx-auto text-[#b4b4b4]">
-            Close
-        </button>
-    </div>
-  )
+            {/* Content */}
+            <section className="h-[100px] my-2 flex justify-evenly items-center flex-row w-full">
+                {/* Icon */}
+                <span className="w-[65px] h-[65px] min-w-[65px]">
+                    <Image src={alert} alt='alert' className='object-cover' width={100} height={100} />
+                </span>
+                {/* Message */}
+                <p className="font-windows">{message}</p>
+            </section>
+            {/* Button */}
+            <button className="button-v2 mx-auto text-[#b4b4b4]">
+                Close
+            </button>
+        </div>
+    )
 }
 
 export default AlertDialogue
