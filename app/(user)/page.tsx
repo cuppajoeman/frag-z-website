671d3b8da4fff767dc2189bf32ff794e81f14625
MichOchieng <MichOchieng@gmail.com>
2023-05-21 04:33:49 -0700

    Added scrollbar back and changed styling to properties window

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -3,7 +3,7 @@ import { PropertiesWindow } from "@/components/PropertiesWindow";
 
 export default function HomePage() {
   return (
-    <main className="h-full min-h-fit overflow-y-scroll overflow-x-clip no-scrollbar flex flex-col items-center justify-start">
+    <main className="h-full min-h-fit overflow-y-scroll overflow-x-clip  flex flex-col items-center justify-start">
       {/* Hero */}
       <Hero/>
       {/* Properties window */}
