df45baed7ffd657da7571b7dc4f9de2a1408f820
MichOchieng <MichOchieng@gmail.com>
2023-05-21 15:45:32 -0700

    Added function to get wiki articles

--- components/supabase/supabase-auth-provider.tsx
+++ components/supabase/supabase-auth-provider.tsx
@@ -13,6 +13,7 @@ type AuthContext = {
     error: any;
     isLoading: boolean;
     mutate: any;
+    getWikiArticles: () => Promise<any>;
     signOut: () => Promise<void>
     signInWithGmail: () => Promise<void>;
     signInWithEmail: (email: string, password: string) => Promise<string | null> ;
@@ -26,6 +27,7 @@ const Context = createContext<AuthContext>({
     error: null,
     isLoading: true,
     mutate: null,
+    getWikiArticles: async () => [],
     signOut: async () => {},
     signInWithGmail: async () => {},
     signInWithEmail: async (email: string, password: string) => null,
@@ -67,6 +69,20 @@ export default function SupabaseAuthProvider({
         isLoading,
         mutate,
     } = useSwr(serverSession ? "profile-context": null, getUser)
+
+    const getWikiArticles =  async () => {
+        console.log("Getting wiki articles...");
+        const { data: articles, error } = await supabase
+            .from("wiki_articles")
+            .select("*")
+        if(error){
+            console.log("Error fetching wiki articles!");
+        }else{
+            console.log("Fetched wiki articles!");
+            return articles
+        }
+    }
+
 
     // signup stuff
 
@@ -151,7 +167,8 @@ export default function SupabaseAuthProvider({
         signInWithDiscord,
         signInWithGmail,
         signInWithEmail,
-        signUpWithEmail
+        signUpWithEmail,
+        getWikiArticles
     };
 
     return (
