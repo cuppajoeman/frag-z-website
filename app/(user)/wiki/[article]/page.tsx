c465bcc5af2a7257a661fb42c34fb488844fda64
Mich Ochieng <michochieng@gmail.com>
2023-06-19 17:43:13 -0700

    Styling fixes and removed newline function

--- app/(user)/wiki/[article]/page.tsx
+++ app/(user)/wiki/[article]/page.tsx
@@ -47,32 +47,6 @@ const regexp_text_line = /^[ \t]*[^\s]+/gm;
 const regexp_code_line = /^[ \t]*(\`{3})/gm;
 let isInCodeBlock = false;
 
-export function add_newlines_to_markdown(text: string): string {
-  const new_lines: string[] = [];
-  const lines = text.split("\n");
-  lines.forEach((line, i) => {
-    line = line.trimEnd();
-    if (i > 0) {
-      const previous_line = lines[i - 1];
-      if (regexp_code_line.test(line)) isInCodeBlock = !isInCodeBlock;
-      if (
-        previous_line.match(regexp_text_line) &&
-        !previous_line.match(regexp_list_line) &&
-        !isInCodeBlock
-      ) {
-        if (line.match(regexp_list_line)) {
-          new_lines[i - 1] = previous_line + "\n\n";
-        } else if (line.match(regexp_text_line) && isInCodeBlock) {
-          new_lines[i - 1] = previous_line + "<br />  ";
-        }
-      }
-    }
-    new_lines.push(line);
-  });
-  const new_text = new_lines.join("\n");
-  return new_text;
-}
-
 export default async function WikiArticle({ params: { article } }: Props) {
   const supabase = createClient();
   const { data } = await supabase
@@ -81,11 +55,11 @@ export default async function WikiArticle({ params: { article } }: Props) {
     .eq("slug", article)
     .single();
 
-  const text = add_newlines_to_markdown(data!.content);
+  const text = data!.content;
 
   return (
     <section className="flex flex-col items-center justify-center h-full min-h-fit w-full ">
-      <span className="w-[350px] sm:w-[600px] sm:my-8 font-windows md:w-[700px] h-[600px] min-h-[600px] sm:h-[700px] sm:min-h-[700px] border-2 border-b-black border-r-black bg-[#C5C5C5] flex flex-col items-start justify-start p-1">
+      <span className="w-[300px] sm:w-[600px] my-4 sm:my-8 font-windows md:w-[700px] h-[600px] min-h-[600px] sm:h-[700px] sm:min-h-[700px] border-2 border-b-black border-r-black bg-[#C5C5C5] flex flex-col items-start justify-start p-1">
         {/* Tab gradient */}
         <div className="tab-gradient">
           <h1 className="">Note</h1>
