82051d8cfba6547313de2eedaee1c299fe9eecdc
MichOchieng <MichOchieng@gmail.com>
2023-07-25 03:26:01 -0700

    Fixed dynamic paths

--- components/FileSystem.tsx
+++ components/FileSystem.tsx
@@ -1,9 +1,12 @@
+'use client';
 import React from 'react'
 import fileIcon from '@/public/appIcons/wikiicon.png'
 import folderIcon from '@/public/appIcons/folder.png'
 import Image from 'next/image'
 import { FileProps, FolderProps } from '@/types'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+
 
 
 function getFolderName(id:number) {
@@ -23,39 +26,41 @@ function getFolderName(id:number) {
     }
 }
 
-const WikiFile = ({ title, created_at, authors }: FileProps) => (
-    <Link 
-        href={`/wiki/${title.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')}`}
-        className="w-full h-[50px] flex flex-row items-center justify-start bg-[#C5C5C5] border-2 border-b-black border-r-black hover:scale-105">
-        {/* img */}
-        <div className="w-[50px] h-[50px] flex items-center justify-center">
-            <Image src={fileIcon} alt="fileIcon" width={100} height={100} />
-        </div>
-        <div className="w-full h-full flex flex-row items-center justify-start font-windows">
-            {/* Title */}
-            <h1 className="mx-auto font-bold">{title}</h1>
-            {/* Authors */}
-            <h1 className="mx-auto">{authors}</h1>
-        </div>
-    </Link>
-)
-
-const Folder = ({ child_directory_id, created_at }: FolderProps) => (
-    <Link 
-        href={`/wiki/${getFolderName(child_directory_id).toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')}`}
-        className="w-full h-[50px] flex flex-row items-center justify-start bg-[#C5C5C5] border-2 border-b-black border-r-black hover:scale-105">
-        {/* img */}
-        <div className="w-[50px] h-[50px] flex items-center justify-center">
-            <Image src={folderIcon} alt="fileIcon" width={100} height={100} />
-        </div>
-        <div className="w-full h-full flex flex-row items-center justify-start font-windows">
-            {/* Title */}
-            <h1 className="mx-auto font-bold">{getFolderName(child_directory_id)}</h1>
-        </div>
-    </Link>
-)
-
 const FileSystem = (articles: any) => {
+    const pathname = usePathname();
+
+    const WikiFile = ({ title, created_at, authors }: FileProps) => (
+        <Link 
+            href={`${pathname}/${title.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')}`}
+            className="w-full h-[50px] flex flex-row items-center justify-start bg-[#C5C5C5] border-2 border-b-black border-r-black hover:scale-105">
+            {/* img */}
+            <div className="w-[50px] h-[50px] flex items-center justify-center">
+                <Image src={fileIcon} alt="fileIcon" width={100} height={100} />
+            </div>
+            <div className="w-full h-full flex flex-row items-center justify-start font-windows">
+                {/* Title */}
+                <h1 className="mx-auto font-bold">{title}</h1>
+                {/* Authors */}
+                <h1 className="mx-auto">{authors}</h1>
+            </div>
+        </Link>
+    )
+    
+    const Folder = ({ child_directory_id, created_at }: FolderProps) => (
+        <Link 
+            href={`${pathname}/${getFolderName(child_directory_id).toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')}`}
+            className="w-full h-[50px] flex flex-row items-center justify-start bg-[#C5C5C5] border-2 border-b-black border-r-black hover:scale-105">
+            {/* img */}
+            <div className="w-[50px] h-[50px] flex items-center justify-center">
+                <Image src={folderIcon} alt="fileIcon" width={100} height={100} />
+            </div>
+            <div className="w-full h-full flex flex-row items-center justify-start font-windows">
+                {/* Title */}
+                <h1 className="mx-auto font-bold">{getFolderName(child_directory_id)}</h1>
+            </div>
+        </Link>
+    )
+
 
     return (
         <span className="properties-window">
