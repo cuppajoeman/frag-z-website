a4ef0362f4b28c0f3f4d584735641b749aecdfff
MichOchieng <MichOchieng@gmail.com>
2023-07-25 02:28:56 -0700

    Added folder support

--- components/FileSystem.tsx
+++ components/FileSystem.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import fileIcon from '@/public/appIcons/wikiicon.png'
+import folderIcon from '@/public/appIcons/folder.png'
 import Image from 'next/image'
-import { FileProps } from '@/types'
+import { FileProps, FolderProps } from '@/types'
 import Link from 'next/link'
 
+
+function getFolderName(id:number) {
+    switch (id) {
+        case 1:   
+            return "Weapons";
+        case 2:   
+            return "Development";
+        case 3:   
+            return "/";
+        case 4:   
+            return "Gamemodes";
+        case 5:   
+            return "Commands";
+        default:
+            return "ERROR";
+    }
+}
 
 const WikiFile = ({ title, created_at, authors }: FileProps) => (
     <Link 
@@ -18,6 +36,21 @@ const WikiFile = ({ title, created_at, authors }: FileProps) => (
             <h1 className="mx-auto font-bold">{title}</h1>
             {/* Authors */}
             <h1 className="mx-auto">{authors}</h1>
+        </div>
+    </Link>
+)
+
+const Folder = ({ child_directory_id, created_at }: FolderProps) => (
+    <Link 
+        href={`/wiki/${getFolderName(child_directory_id).toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')}`}
+        className="w-full h-[50px] flex flex-row items-center justify-start bg-[#C5C5C5] border-2 border-b-black border-r-black hover:scale-105">
+        {/* img */}
+        <div className="w-[50px] h-[50px] flex items-center justify-center">
+            <Image src={folderIcon} alt="fileIcon" width={100} height={100} />
+        </div>
+        <div className="w-full h-full flex flex-row items-center justify-start font-windows">
+            {/* Title */}
+            <h1 className="mx-auto font-bold">{getFolderName(child_directory_id)}</h1>
         </div>
     </Link>
 )
@@ -39,7 +72,7 @@ const FileSystem = (articles: any) => {
                 {/* Explorer*/}
                 <div className="w-full h-fit min-h-full overflow-y-scroll no-scrollbar flex flex-col p-5 items-start justify-start border-2 border-t-black border-l-black bg-white">
                     {articles.articles.map((article: any) =>
-                        <WikiFile key={article.id} {...article} />
+                        (article.parent_dir_id === null ? <WikiFile key={article.id} {...article} /> : <Folder key={article.id} {...article} />)
                     )}
                 </div>
             </section>
