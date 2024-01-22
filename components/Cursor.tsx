0cca7fbe497cc45198d7d2bb6d0c48ec60b73e1a
ccn <ccn@cuppajoeman.com>
2024-01-21 22:41:15 -0500

    improve smoke effect

--- components/Cursor.tsx
+++ components/Cursor.tsx
@@ -5,17 +5,28 @@ import { characterCursor } from "cursor-effects";
 const Cursor = () => {
     const test = new characterCursor({ 
         element: document.querySelector("#character"), 
-        characters: ["*"],
+        characters: [ // For each color you need a character, this is a bug in my original pr.
+          "*",
+          "*",
+          "*",
+          "*",
+        ] ,
         font: "15px serif",
         colors: [
             "#111",
+            "#bd7c0b",
+            "#781708",
+            "#9e1803",
         ],
+        cursorOffset: {
+          x : 24, y : 24
+        },
         characterLifeSpanFunction: function() {
             return Math.floor(Math.random() * 60 + 80);
         },
         initialCharacterVelocityFunction: function() {
             return {
-                x: (Math.random() < 0.5 ? -1 : 1) * Math.random() / 2,
+                x: 1 + (Math.random() < 0.5 ? -1 : 1) * Math.random() / 2,
                 y: 1 + Math.random(),
             }
         },
@@ -29,7 +40,7 @@ const Cursor = () => {
         },
         characterScalingFunction: function(age:number, lifeSpan:number) {
             let lifeLeft = lifeSpan - age;
-            return Math.max(lifeLeft / lifeSpan * 2, 0);
+            return Math.max(lifeLeft / lifeSpan , 0) * 1.5;
         },
         characterNewRotationDegreesFunction: function(age:number, lifeSpan:number) {
             let lifeLeft = lifeSpan - age;
